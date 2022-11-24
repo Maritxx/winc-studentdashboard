193eea1e529873b264f1f32177ebf910cbb7d99f
Marit de Vries <Maritdevries1@live.nl>
2022-11-24 17:13:28 +0100

    Set-up store and initialized state studentData

---
+++ src/Redux/reducers/studentDataReducer.js
@@ -0,0 +1,9 @@
+import { studentData } from '../../data';
+
+const initialState = studentData;
+
+const studentDataReducer = (state = initialState, action) => {
+    return state
+};
+
+export default studentDataReducer;
